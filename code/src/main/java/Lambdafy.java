8e594a45f0d97827681e1628b11de5e99006580a
Gregor Trefs <Gregor.Trefs@gmail.com>
2016-07-13 01:09:52 +0200

    Added slides about invokeDynamic

--- code/src/main/java/Lambdafy.java
+++ code/src/main/java/Lambdafy.java
@@ -4,6 +4,7 @@ public class Lambdafy {
 
     public static void main(String[] args){
         final Function<String, String> f = a -> a;
+        f.apply("hello");
     }
 
 //    Compiled from "Lambdafy.java"
@@ -16,13 +17,17 @@ public class Lambdafy {
 //
 //        public static void main(java.lang.String[]);
 //        Code:
-//                0: invokedynamic #2,  0              // InvokeDynamic #0:apply:()Ljava/util/function/Function;
-//                5: astore_1
-//                6: return
+//                0: invokedynamic #2,  0              // InvokeDynamic #0:apply:()Ljava/util/function/Function; create new function instance
+//                5: astore_1                          // store function instance in local variable 1
+//                6: aload_1                           // load  onto the stack
+//                7: ldc           #3                  // String hello; push constant on the stack
+//                9: invokeinterface #4,  2            // InterfaceMethod java/util/function/Function.apply:(Ljava/lang/Object;)Ljava/lang/Object;
+//                14: pop
+//                15: return
 //
 //        private static java.lang.String lambda$main$0(java.lang.String);
 //        Code:
-//                0: aload_0
-//                1: areturn
+//                0: aload_0 // load reference of local variable 0 onto the stack
+//                1: areturn // take reference form stack and return it
 //    }
 }
